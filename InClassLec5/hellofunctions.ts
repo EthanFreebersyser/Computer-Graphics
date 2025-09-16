47ce7056b754789eae17d8a480867a09e691ef5b
Ethan Freebersyser <etrepfre@gmail.com>
2025-09-16 02:06:43 -0500

    Working version!

---
+++ InClassLec5/hellofunctions.ts
@@ -0,0 +1,161 @@
+import {initShaders, vec4, flatten} from "../helperfunctions.js";
+
+"use strict";
+//we will want references to our WebGL objects
+let gl:WebGLRenderingContext;
+let canvas:HTMLCanvasElement;
+let program:WebGLProgram;
+
+let color:vec4; //this one lives in main memory
+let ucolor:WebGLUniformLocation; //store location of shader uniform
+let xoffset:number;
+let yoffset:number;
+let bufferId: WebGLBuffer;
+let trianglePoints:vec4[] = [];  //empty array in main memory
+
+let newUpdate:boolean = false;
+
+//set up program when page first loads
+window.onload = function init(){
+    canvas = document.getElementById("gl-canvas") as HTMLCanvasElement;
+    gl = canvas.getContext('webgl2') as WebGLRenderingContext;
+    if (!gl){
+        alert("WebGL isn't available");
+    }
+
+    //take the vertex and fragment shaders and compile them into a shader program
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    color = new vec4(1,1,1,1);//rgba for white
+    ucolor = gl.getUniformLocation(program, "color");
+    gl.uniform4fv(ucolor, color.flatten());
+
+    //set up keyboard listener
+    window.addEventListener("keydown", keyDownListener);
+
+    canvas.addEventListener("mousedown", mouseDownListener);
+
+    xoffset = 0;
+    yoffset = 0;
+
+    makeTriangleAndBuffer();
+
+    //we'll cover what this is in a future lecture
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    //what color is the background?
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    //request that a frame be drawn right now
+    requestAnimationFrame(render); //provide the name of your render function
+    window.setInterval(update, 16);
+}
+
+function update(){
+    if (newUpdate){
+        let tempTrianglePoints:vec4[] = trianglePoints;
+        trianglePoints = []
+        for (let i: number = 0; i < tempTrianglePoints.length; i++){
+            if (tempTrianglePoints[i][0] + 0.01 <= -1){
+                trianglePoints.push(new vec4(tempTrianglePoints[i][0] + 0.01, tempTrianglePoints[i][1] + 0.01, tempTrianglePoints[i][2], tempTrianglePoints[i][3]));
+            } else if(tempTrianglePoints[i][0] + 0.01 >= 1){
+                trianglePoints.push(new vec4(tempTrianglePoints[i][0] - 0.01, tempTrianglePoints[i][1] - 0.01, tempTrianglePoints[i][2], tempTrianglePoints[i][3]));
+            }
+            
+        }
+    }
+
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(trianglePoints), gl.STATIC_DRAW);
+
+    requestAnimationFrame(render);
+}
+
+function keyDownListener(event:KeyboardEvent){
+    switch (event.key) {
+        case "r":
+            color = new vec4(1, 0, 0, 1); //red
+            break;
+        case "g":
+            color = new vec4(0, 1, 0, 1); //green
+            break;
+        case "b":
+            color = new vec4(0, 0, 1, 1); //blue
+            break;
+        case "c":
+            color = new vec4(Math.random(), Math.random(), Math.random(), 1);
+            break;
+        case " ":
+            newUpdate = !newUpdate;
+            break;
+    }
+    //send main memory value over as uniform value
+    gl.uniform4fv(ucolor, color.flatten());
+    requestAnimationFrame(render); //we need a new frame, render() is our drawing function
+}
+
+function mouseDownListener(event:MouseEvent){
+    let rect:ClientRect = canvas.getBoundingClientRect(); //canvas has spacing to edge of window
+    let canvasY:number = event.clientY - rect.top; //window -> canvas coordinates
+    let flippedY:number = canvas.clientHeight - canvasY; //openGL wants origin lower left
+
+    //need to keep things between -1 and 1 for default opengl view volume (for now)
+    yoffset = 2 * flippedY / canvas.clientHeight - 1;
+    //no need to flip x coordinate backwards
+    xoffset = 2 * (event.clientX - rect.left)/canvas.clientWidth - 1;
+
+    /////////////////////////////////////////////////
+    //READ THIS !!!!!!! READ THIS!!!!!
+    // DON'T DO THIS AFTER PROJECT 1!  IT'S BAD!!
+    // NO NONONONONONONONO
+    ////////////////////////////////////////////////
+    let trianglePoints = []; //empty array
+    //create 3 new triangle verts altered by mouse click offsets
+    trianglePoints.push(new vec4(-0.5 + xoffset, -0.5 + yoffset, 0, 1));
+    trianglePoints.push(new vec4(0 + xoffset, 0.5 + yoffset, 0, 1));
+    trianglePoints.push(new vec4(0.5 + xoffset, -0.5 + yoffset, 0, 1));
+    //just in case we have more than one buffer...
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    //send over this new vertex data
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(trianglePoints), gl.STATIC_DRAW);
+    // END BAD STUFF
+    ////////////////////////////////////////////////////
+    requestAnimationFrame(render); //we're ready for a new image to be drawn with this info
+}
+
+function makeTriangleAndBuffer(){
+
+    //create 3 vertices and add to main memory array
+    //stay between -1 and 1 until we cover projection
+    trianglePoints.push(new vec4(-0.5, -0.5, 0, 1));
+    trianglePoints.push(new vec4(0, 0.5, 0, 1));
+    trianglePoints.push(new vec4(0.5, -0.5, 0, 1));
+
+    //need some graphics memory allocated
+    bufferId = gl.createBuffer();
+    //tell WebGL that the buffer we just created is the one we want to work with
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    //send main memory data to video memory.  Note use of Angel's flatten
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(trianglePoints), gl.STATIC_DRAW);
+
+    //What is this data going to be used for?
+    //The vertex shader code had an attribute named "vPosition".  This data feeds that
+    let vPosition:GLint = gl.getAttribLocation(program, "vPosition"); //fetch by name
+    //attrib location we just fetched, 4 elements per value, data type float, don't normalize
+    //data has no gaps, starts righta way at index 0
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+}
+
+//draw one frame
+function render(){
+    //start by clearing any buffers
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //if needed...
+    //gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+
+    //draw our geometry
+    //draw triangles, start at index 0, there will be 3 verts total
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
--- Project 1/hellofunctions.ts
+++ Project 1/hellofunctions.ts
@@ -5,6 +5,22 @@ let gl:WebGLRenderingContext;
 let canvas:HTMLCanvasElement;
 let program:WebGLProgram;
 let bufferId:WebGLBuffer;
+let crosshairBuffer:WebGLBuffer;
+
+type Target = {
+    x: number; y: number;
+    vx: number; vy: number;
+    size: number;
+    alive: boolean;
+    color: vec4;
+};
+
+let targets: Target[] = [];
+let motionEnabled: boolean = false;
+let lastTime = performance.now();
+let shotsFired: number = 0;
+let aimX: number = 0, aimY: number = 0;
+
 
 //do this when the page first loads
 window.onload = function(){
@@ -18,7 +34,17 @@ window.onload = function(){
     program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    makeTriangleAndBuffer();
+    bufferId = gl.createBuffer()!;
+    crosshairBuffer = gl.createBuffer()!;
+
+    window.addEventListener("keydown", keyDownListener);
+
+    canvas.addEventListener("mousedown", mouseDownListener);
+
+    canvas.addEventListener("mousemove", mouseMoveListener);
+
+    (document.getElementById("resetBtn") as HTMLButtonElement).addEventListener("click", () => {resetGame();});
+
 
     //what part of the canvas should we use (all of it here)
     gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
@@ -26,53 +52,189 @@ window.onload = function(){
     gl.clearColor(0.0, 0.0, 0.0, 1.0); //start out opaque black
 
     //request a frame to be drawn
-    requestAnimationFrame(render); //render is the name of our drawing function below
+    resetGame()
+    lastTime = performance.now();
+    window.setInterval(gameLoop, 16);
 }
 
-function makeTriangleAndBuffer(){
-    let trianglePoints:vec4[] = []; //create a local (RAM) array
+function keyDownListener(event:KeyboardEvent){
+    switch (event.key) {
+        case "m":
+            motionEnabled = !motionEnabled;
+            break;
+    }
+    updateFeedback();
+}
 
-    //create three vertices and add to local array
-    //we haven't discussed projection. so stay between -1 to 1
-    trianglePoints.push(new vec4(-0.5, -0.5, 0.0, 1.0)); //vertex 1
-    trianglePoints.push(new vec4(1, 0, 0, 1));  //opaque red
+function mouseMoveListener(event: MouseEvent){
+    const rect = canvas.getBoundingClientRect();
+    const cx = (event.clientX - rect.left) / rect.width;  // 0..1
+    const cy = (event.clientY - rect.top) / rect.height;  // 0..1
+    aimX = cx*2 - 1;
+    aimY = 1 - cy*2;
+    rebuildCrosshairBuffer();
+}
 
-    trianglePoints.push(new vec4(0, 0.5, 0.0, 1.0)); //vertex 2
-    trianglePoints.push(new vec4(0, 1, 0, 1)); // opaque green
+function mouseDownListener(event:MouseEvent){
+    shotsFired++;
+    let anyHit:boolean = false;
+    for (const target of targets){
+        if (!target.alive) continue;
+        const hit:boolean = Math.abs(aimX - target.x) <= target.size && Math.abs(aimY - target.y) <= target.size;
+        if (hit) {
+            target.alive = false;
+            anyHit = true;
+        }
+    }
+    if (anyHit) {
+        rebuildBuffer();
+    }
+    updateFeedback();
+}
 
-    trianglePoints.push(new vec4(0.5, -0.5, 0.0, 1.0)); //vertex 3
-    trianglePoints.push(new vec4(0, 0, 1, 1)); // opaque blue
+function resetGame() {
+    let numOfTargets: number = getRandomInt(5, 10);
+    targets = [];
+    for (let i = 0; i < numOfTargets; i++) {
+        const size = 0.05;
 
-    //get some graphics card memory
-    bufferId = gl.createBuffer();
-    //tell WebGl that the buffer we just created is the one to work with now
+        const x = (Math.random() * 2 - 1) * (1 - size);
+        const y = (Math.random() * 2 - 1) * (1 - size);
+
+        const angle = (Math.random() * Math.PI) * 2;
+        const speed = 0.25 + (Math.random() * 0.35);
+        const vx = Math.cos(angle) * speed;
+        const vy = Math.sin(angle) * speed;
+
+        targets.push({
+            x, y, vx, vy, size,
+            alive: true,
+            color: new vec4(Math.random(), Math.random(), Math.random(), 1),
+        })
+    }
+    shotsFired = 0;
+    motionEnabled = false;
+    rebuildBuffer();
+    updateFeedback();
+    rebuildCrosshairBuffer();
+}
+
+
+function pushTriangle(pts: vec4[], ptx: number, pty: number, ptSize: number, color: vec4){
+    const x0: number = ptx - ptSize, x1: number = ptx + ptSize;
+    const y0: number = pty - ptSize, y1: number = pty + ptSize;
+    //this is fancy
+    const xAndy = (x: number, y: number) => new vec4(x,y,0,1);
+
+    //triangle 1
+    pts.push(xAndy(x0,y0), color, xAndy(x1,y0), color, xAndy(x1,y1), color);
+    //triangle 2
+    pts.push(xAndy(x0,y0), color, xAndy(x1,y1), color, xAndy(x0,y1), color);
+}
+
+function rebuildBuffer(){
+    const trianglePoints: any[] = [];
+    for (const target of targets){
+        if (!target.alive) continue;
+        pushTriangle(trianglePoints, target.x, target.y, target.size, target.color);
+    }
+
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    //send the local data over to the graphics card
-    //flatten converts to ID array
     gl.bufferData(gl.ARRAY_BUFFER, flatten(trianglePoints), gl.STATIC_DRAW);
 
-    //Data is packed in groups of 4 floats which are 4 bytes each, 32 bytes total for position and color
-    // position             color
-    // x    y    z    w       r     g     b     a
-    // 0-3 4-7 8-11 12-15   16-19 20-23 24-27 28-31
-
-    //tell openGl what the data means
-    let vPosition:GLint = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 32, 0);
-    gl.enableVertexAttribArray(vPosition);
-
-    let vColor:GLint = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 32, 16);
-    gl.enableVertexAttribArray(vColor);
 }
+
+function rebuildCrosshairBuffer(){
+    const s = 0.03;
+    const color = new vec4(1,1,1,1);
+    const xAndy = (x:number, y:number) => new vec4(x,y,0,1);
+
+    const verts:any[] = [
+        xAndy(aimX - s, aimY), color, xAndy(aimX + s, aimY), color,
+        xAndy(aimX, aimY - s), color, xAndy(aimX, aimY + s), color,
+    ];
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, crosshairBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(verts), gl.STATIC_DRAW);
+
+}
+
+//update the feedback text field
+function updateFeedback(){
+    //filter is an awesome function
+    const alive: number = targets.filter(t => t.alive).length;
+    const txt = document.getElementById("feedback");
+    if (alive > 0) {
+        txt.textContent = `Targets left ${alive} - Press 'm' to ${motionEnabled ? 'stop' : 'start'} motion - Shots: ${shotsFired}`;
+    } else {
+        txt.textContent = `All targets down! Shots: ${shotsFired}. Click Reset to play again.`;
+    }
+}
+
+function update(dt: number){
+    if (!motionEnabled) return;
+
+    for (const target of targets){
+        if (!target.alive) continue;
+        target.x += target.vx * dt;
+        target.y += target.vy * dt;
+
+        //bounce
+        if(target.x + target.size > 1) {target.x = 1 - target.size; target.vx *= -1;}
+        if(target.x - target.size < -1) {target.x = -1 + target.size; target.vx *= -1;}
+        if(target.y + target.size > 1) {target.y = 1 - target.size; target.vy *= -1;}
+        if(target.y - target.size < -1) {target.y = -1 + target.size; target.vy *= -1;}
+    }
+    rebuildBuffer();
+}
+
+function gameLoop(){
+    const now = performance.now();
+    const dt = (now - lastTime) / 1000;
+    lastTime = now;
+
+    update(dt);
+    render();
+}
+
+
 
 function render(){
     //start by clearing all buffers
     gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
 
-    //if we needed to we could bind to the correct drawing buffer
-    //but if we're already bout to it, this would have no impact
-    //gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    const vPosition = gl.getAttribLocation(program, 'vPosition');
+    const vColor = gl.getAttribLocation(program, 'vColor');
 
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    //draw targets
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 32, 0);
+    gl.enableVertexAttribArray(vPosition);
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 32, 16);
+    gl.enableVertexAttribArray(vColor);
+
+    let liveVerts: number = 0;
+    for (const target of targets){
+        if (target.alive) {
+            liveVerts += 6;
+        }
+    }
+    gl.drawArrays(gl.TRIANGLES, 0, liveVerts);
+
+    //draw crosshair
+    gl.bindBuffer(gl.ARRAY_BUFFER, crosshairBuffer);
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 32, 0);
+    gl.enableVertexAttribArray(vPosition);
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 32, 16);
+    gl.enableVertexAttribArray(vColor);
+
+
+    gl.drawArrays(gl.LINES, 0,4);
+}
+
+function getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
