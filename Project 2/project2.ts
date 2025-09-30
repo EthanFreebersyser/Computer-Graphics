5e1a0e28800b1e4ac8c7812ddbc693cb6f0cb405
Ethan Freebersyser <etf43459@bethel.edu>
2025-09-30 00:00:02 -0500

    Wheel model added

--- Project 2/project2.ts
+++ Project 2/project2.ts
@@ -14,7 +14,7 @@ let vColor:GLint; //color shader attribute loc
 
 //TODO Add various animation parameters
 
-import {initShaders, vec4, mat4, flatten, perspective, translate, lookAt, rotateX, rotateY, rotateZ, scalem} from './helperfunctions.js';
+import {initShaders, vec4, vec3, mat4, flatten, perspective, translate, lookAt, rotateX, rotateY, rotateZ, scalem} from './helperfunctions.js';
 
 //We want some set up to happen immediately when the page loads
 window.onload = function init() {
@@ -84,9 +84,84 @@ function makeGroundWheelBodyBuffer(){
     //</editor-fold>
 
     //<editor-fold desc="wheel pts">
-    //start wheel 6-?
+    //start wheel 6-96
         let wheelPts:vec4[] = [];
-        //TODO add pts
+
+        const radius: number = 0.25;
+        const width: number = 0.125;
+        const centerTop = new vec4(0, width/2, 0, 1.0);
+        const centerBottom = new vec4(0, -width/2, 0, 1.0);
+        const nTop = new vec3(0, 1, 0);
+        const nBot = new vec3(0,-1, 0);
+        const slices: number = 8;
+
+        // two alternating color
+        const wheelColor1:vec4 = new vec4(1, 0, 0, 1.0);
+        const wheelColor2:vec4 = new vec4(0, 0, 1, 1.0);
+        let colorChanger: boolean = true;
+
+        for (let i: number = 0; i < slices; i++){
+            //angles
+            const angleStart: number = (2 * Math.PI * i) / slices;
+            const angleEnd: number = (2 * Math.PI * (i+1)) / slices;
+
+            //rim x,z locations
+            const rimXStart: number = radius * Math.cos(angleStart);
+            const rimZStart: number = radius * Math.sin(angleStart);
+            const rimXEnd: number = radius * Math.cos(angleEnd);
+            const rimZEnd: number  = radius * Math.sin(angleEnd);
+
+            //rim start and ends
+            const bottomRimStart = new vec4(rimXStart, -width/2, rimZStart,1.0);
+            const bottomRimEnd = new vec4(rimXEnd, -width/2, rimZEnd,1.0);
+            const topRimEnd = new vec4(rimXEnd, width/2, rimZEnd,1.0);
+            const topRimStart = new vec4(rimXStart, width/2, rimZStart,1.0);
+
+            //Side: 2 Triangles
+                //triangle 1
+                wheelPts.push(bottomRimStart);
+                wheelPts.push(wheelColor1);
+                wheelPts.push(bottomRimEnd);
+                wheelPts.push(wheelColor1);
+                wheelPts.push(topRimEnd);
+                wheelPts.push(wheelColor1);
+
+                //triangle 2
+
+                wheelPts.push(bottomRimStart);
+                wheelPts.push(wheelColor2);
+                wheelPts.push(topRimEnd);
+                wheelPts.push(wheelColor2);
+                wheelPts.push(topRimStart);
+                wheelPts.push(wheelColor2);
+
+            //Top Cap
+                //triangle 3
+                let wheelColor: vec4;
+                if (colorChanger){
+                    wheelColor = wheelColor1;
+                    colorChanger = !colorChanger;
+                } else {
+                    wheelColor = wheelColor2
+                    colorChanger = !colorChanger;
+                }
+                wheelPts.push(centerTop);
+                wheelPts.push(wheelColor);
+                wheelPts.push(topRimStart);
+                wheelPts.push(wheelColor);
+                wheelPts.push(topRimEnd);
+                wheelPts.push(wheelColor);
+
+            //Bottom Cap
+                //triangle 4
+                wheelPts.push(centerBottom);
+                wheelPts.push(wheelColor2);
+                wheelPts.push(bottomRimEnd);
+                wheelPts.push(wheelColor2);
+                wheelPts.push(bottomRimStart);
+                wheelPts.push(wheelColor2);
+
+        }
 
         for (let pt of wheelPts) {
             allPts.push(pt);
@@ -104,7 +179,6 @@ function makeGroundWheelBodyBuffer(){
         }
     //end body
     //</editor-fold>
-
 
     //<editor-fold desc="build buffer">
     //start buffer
@@ -154,24 +228,91 @@ function render(){
 
     gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
 
-    //model view matrix
-    //lookAT parames: where is the camera? what is a location the camrea is looking at? what direction is up?
-    let mv:mat4 = lookAt(new vec4(0,5,20,1), new vec4(0,0,0,1), new vec4(0,1,0,0));
+    //<editor-fold desc="Draw Background">
+        //model view matrix
+        //lookAT parames: where is the camera? what is a location the camrea is looking at? what direction is up?
+        let mv:mat4 = lookAt(new vec4(0,5,20,1), new vec4(0,0,0,1), new vec4(0,1,0,0));
 
-    //multiply translate matrix to the right of lookAt matric
-    //TODO add this mv = mv.mult(translate(xOffset,yOffset,zOffset));
-    mv = mv.mult(scalem(10, 1,10))
+        //multiply matrix to the right of lookAt matrix
+        mv = mv.mult(scalem(10, 1,10))
+
+        //send over model view matrix as a uniform
+        gl.uniformMatrix4fv(umv,false, mv.flatten());
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 6);    // draw the background 0-6
+    //</editor-fold>
+
+    //<editor-fold desc="Draw front left wheel>
+        //model view matrix
+        mv = lookAt(new vec4(0,5,20,1), new vec4(0,0,0,1), new vec4(0,1,0,0));
+
+        //multiply matrix to the right of lookAt matrix
+        mv = mv.mult(translate(-1,0,0));
+        mv = mv.mult(scalem(1, 1,1))
+        mv = mv.mult(rotateZ(90))
+        mv = mv.mult(rotateX(1))
+
+        //send over model view matrix as a uniform
+        gl.uniformMatrix4fv(umv,false, mv.flatten());
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+
+        gl.drawArrays(gl.TRIANGLES, 6, 96);
+    //</editor-fold>
+
+    //<editor-fold desc="Draw front right wheel>
+    //model view matrix
+    mv = lookAt(new vec4(0,5,20,1), new vec4(0,0,0,1), new vec4(0,1,0,0));
+
+    //multiply matrix to the right of lookAt matrix
+    mv = mv.mult(translate(1,0,0));
+    mv = mv.mult(rotateZ(90))
+    mv = mv.mult(rotateX(1))
 
     //send over model view matrix as a uniform
     gl.uniformMatrix4fv(umv,false, mv.flatten());
 
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    //TODO draw the geometry we previously sent over.  It's a list of ??? triangle(s),
-    //TODO we want to start at index 0, and there will be a total of ??? vertices (??? faces with ??? vertices each)
 
-    gl.drawArrays(gl.TRIANGLES, 0, 6);    // draw the background
+    gl.drawArrays(gl.TRIANGLES, 6, 96);
+    //</editor-fold>
 
-    //TODO Add each wheel individually and the car body
+    //<editor-fold desc="Draw back right wheel>
+    //model view matrix
+    mv = lookAt(new vec4(0,5,20,1), new vec4(0,0,0,1), new vec4(0,1,0,0));
+
+    //multiply matrix to the right of lookAt matrix
+    mv = mv.mult(translate(1,0,3));
+    mv = mv.mult(rotateZ(90))
+    mv = mv.mult(rotateX(1))
+
+    //send over model view matrix as a uniform
+    gl.uniformMatrix4fv(umv,false, mv.flatten());
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+
+    gl.drawArrays(gl.TRIANGLES, 6, 96);
+    //</editor-fold>
+
+    //<editor-fold desc="Draw back left wheel>
+    //model view matrix
+    mv = lookAt(new vec4(0,5,20,1), new vec4(0,0,0,1), new vec4(0,1,0,0));
+
+    //multiply matrix to the right of lookAt matrix
+    mv = mv.mult(translate(-1,0,3));
+    mv = mv.mult(rotateZ(90))
+    mv = mv.mult(rotateX(1))
+
+    //send over model view matrix as a uniform
+    gl.uniformMatrix4fv(umv,false, mv.flatten());
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+
+    gl.drawArrays(gl.TRIANGLES, 6, 96);
+    //</editor-fold>
+
 }
 
 
